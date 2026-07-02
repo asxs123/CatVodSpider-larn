c8cfb12770e47bd3959439d34d97da2adea52392
asxs123 <54346276+asxs123@users.noreply.github.com>
2026-07-02 19:47:35 +0800

    1

--- app/src/main/java/com/github/catvod/spider/Ikanbot.java
+++ app/src/main/java/com/github/catvod/spider/Ikanbot.java
@@ -10,6 +10,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -25,7 +26,8 @@ import java.util.regex.Pattern;
 
 public class Ikanbot extends Spider {
 
-    private static final String siteUrl = "https://v.ikanbot.com";
+    // private static final String siteUrl = "https://v.ikanbot.com";
+    private static final String siteUrl = "https://www.ikanbot.com/";
     private static final String cateUrl = siteUrl + "/hot";
     private static final String detailUrl = siteUrl + "/play/";
     private static final String searchUrl = siteUrl + "/search?q=";
@@ -40,9 +42,10 @@ public class Ikanbot extends Spider {
         List<Vod> list = new ArrayList<>();
         for (Element element : doc.select("a.item")) {
             String pic = element.select("img").attr("data-src");
-            String url = element.attr("href");
+            // String url = element.attr("href");
             String name = element.select("img").attr("alt");
-            String id = url.split("/")[2];
+            // String id = url.split("/")[2];
+            String id = element.select("img").attr("id");
             list.add(new Vod(id, name, pic));
         }
         return list;
@@ -51,30 +54,38 @@ public class Ikanbot extends Spider {
     @Override
     public String homeContent(boolean filter) throws Exception {
         List<Class> classes = new ArrayList<>();
-        String[] typeIdList = {"/index-movie-热门", "/index-tv-热门", "/index-tv-国产剧", "/index-tv-韩剧"};
-        String[] typeNameList = {"热门电影", "热门剧集", "国产剧", "韩剧"};
+        String[] typeIdList = {"/index-movie-热门", "/index-tv-热门", "/index-tv-综艺", "/index-tv-纪录片", "/index-tv-日本动画"};
+        String[] typeNameList = {"电影", "剧集", "综艺", "纪录片", "动漫"};
         for (int i = 0; i < typeNameList.length; i++) {
             classes.add(new Class(typeIdList[i], typeNameList[i]));
         }
+        String filters = "{\"/index-movie-热门\":[{\"key\":\"key\",\"name\":\"分类\",\"value\":[{\"n\":\"全部\",\"v\":\"/index-movie-热门\"},{\"v\":\"/index-movie-最新\",\"n\":\"最新\"},{\"v\":\"/index-movie-经典\",\"n\":\"经典\"},{\"v\":\"/index-movie-豆瓣高分\",\"n\":\"豆瓣高分\"},{\"v\":\"/index-movie-冷门佳片\",\"n\":\"冷门佳片\"},{\"v\":\"/index-movie-华语\",\"n\":\"华语\"},{\"v\":\"/index-movie-欧美\",\"n\":\"欧美\"},{\"v\":\"/index-movie-韩国\",\"n\":\"韩国\"},{\"v\":\"/index-movie-日本\",\"n\":\"日本\"},{\"v\":\"/index-movie-动作\",\"n\":\"动作\"},{\"v\":\"/index-movie-喜剧\",\"n\":\"喜剧\"},{\"v\":\"/index-movie-爱情\",\"n\":\"爱情\"},{\"v\":\"/index-movie-科幻\",\"n\":\"科幻\"},{\"v\":\"/index-movie-悬疑\",\"n\":\"悬疑\"},{\"v\":\"/index-movie-恐怖\",\"n\":\"恐怖\"},{\"v\":\"/index-movie-动画\",\"n\":\"动画\"},{\"v\":\"/index-movie-豆瓣top250\",\"n\":\"豆瓣top250\"}]}],\"/index-tv-热门\":[{\"key\":\"key\",\"name\":\"分类\",\"value\":[{\"v\":\"/index-tv-热门\",\"n\":\"全部\"},{\"v\":\"/index-tv-美剧\",\"n\":\"美剧\"},{\"v\":\"/index-tv-英剧\",\"n\":\"英剧\"},{\"v\":\"/index-tv-韩剧\",\"n\":\"韩剧\"},{\"v\":\"/index-tv-日剧\",\"n\":\"日剧\"},{\"v\":\"/index-tv-国产剧\",\"n\":\"国产剧\"},{\"v\":\"/index-tv-港剧\",\"n\":\"港剧\"}]}]}";
         Document doc = Jsoup.parse(OkHttp.string(siteUrl + "/billboard.html", getHeaders()));
         List<Vod> list = new ArrayList<>();
         for (Element element : doc.select("div.item-root")) {
             String pic = element.select("img").attr("data-src");
-            String url = element.select("a").attr("href");
+            // String url = element.select("a").attr("href");
+            String url = element.select("img").attr("id");
             String name = element.select("img").attr("alt");
-            try {
-                String id = url.split("/")[2];
-                list.add(new Vod(id, name, pic));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            // try {
+            //     String id = url.split("/")[2];
+            //     list.add(new Vod(id, name, pic));
+            // } catch (Exception e) {
+            //     e.printStackTrace();
+            // }
+            list.add(new Vod(url, name, pic, "vod_remarks", "vod_actor"));
         }
-        return Result.string(classes, list);
+        return Result.string(classes, list, filter ? JsonParser.parseString(filters) : null);
     }
 
     @Override
     public String categoryContent(String tid, String pg, boolean filter, HashMap<String, String> extend) throws Exception {
-        String target = cateUrl + tid;
+        String target = "";
+        if (extend != null && !extend.isEmpty()) {
+            target = cateUrl + extend.get("key");
+        } else {
+            target = cateUrl + tid;
+        }
         if (!"1".equals(pg)) {
             target = target + "-p-" + pg;
         }
@@ -139,12 +150,16 @@ public class Ikanbot extends Spider {
         vod.setVodName(name);
         vod.setVodPlayFrom(PlayFrom);
         vod.setVodPlayUrl(PlayUrl.replace("##", "#").replace("#$$$", "$$$"));
+        vod.setTypeName("type_name");
+        vod.setVodRemarks("vod_remarks");
+        vod.setVodDirector("vod_director");
+        vod.setVodContent("vod_content");
         return Result.string(vod);
     }
 
     @Override
     public String searchContent(String key, boolean quick) throws Exception {
-        Document doc = Jsoup.parse(OkHttp.string(searchUrl.concat(URLEncoder.encode(key)), getHeaders()));
+        Document doc = Jsoup.parse(OkHttp.string(searchUrl.concat(URLEncoder.encode(key, "UTF-8")), getHeaders()));
         List<Vod> list = new ArrayList<>();
         for (Element element : doc.select("a.cover-link")) {
             String pic = element.select("img").attr("data-src");
